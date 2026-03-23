9b90590e009af8e115c26347d4dcf5496653aa3a
Hardy <2273752107@qq.com>
2026-03-23 15:20:31 +0800

    Initial commit

---
+++ src/test/java/person/hardy/gguide/GguideApplicationTests.java
@@ -0,0 +1,13 @@
+package person.hardy.gguide;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GguideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
