81aaf5b29df11576fbc66acacfcf686687394f6a
coder-Hardy <2273752107@qq.com>
2026-03-24 23:19:39 +0800

    fix:代码启动修改

--- src/main/java/person/hardy/gguide/model/vo/AuthResponseVO.java
+++ src/main/java/person/hardy/gguide/model/vo/AuthResponseVO.java
@@ -1,25 +1,15 @@
 package person.hardy .gguide.model.vo;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class AuthResponseVO {
     private String message;
     private String userId;
     private String token;
 
-    // 构造方法
-    public static AuthResponseVO success(String message, String userId) {
-        AuthResponseVO vo = new AuthResponseVO();
-        vo.setMessage(message);
-        vo.setUserId(userId);
-        return vo;
-    }
-
-    public static AuthResponseVO loginSuccess(String message, String token) {
-        AuthResponseVO vo = new AuthResponseVO();
-        vo.setMessage(message);
-        vo.setToken(token);
-        return vo;
-    }
 }
--- src/test/java/person/hardy/gguide/GguideApplicationTests.java
+++ src/test/java/person/hardy/gguide/GguideApplicationTests.java
@@ -3,7 +3,10 @@ package person.hardy.gguide;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+	"app.seed.enabled=false",
+	"spring.data.mongodb.auto-index-creation=false"
+})
 class GguideApplicationTests {
 
 	@Test
